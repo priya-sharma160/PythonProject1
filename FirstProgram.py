62d3cf0d0a4c8b38db9a8e3ae5a59f4a9fcee15d
Priya Sharma <priya@smartsolution.dev>
2025-08-22 13:21:36 +0300

    Initial commit

---
+++ FirstProgram.py
@@ -0,0 +1,18 @@
+'''
+from multiprocessing.managers import SharedMemoryManager
+#print('It\'s nice to meet you')
+#print("My name is Priya Sharma\U0001F605")
+#emoji(Control+Command+space)
+#print("Hello!\nHow are you?")#nextline
+#print("Hello!\tHow are you?")#tab
+
+(name = "Priya"
+surname = "Sharma"
+fullname = name + " " + surname
+print(fullname))
+name = "My name is Priya"
+surname = "My surname is Sharma"
+
+print(name,'=' * 83, surname)
+
+'''
