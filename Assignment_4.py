a4da55ecc92063f7184549e5de1c71808ad9fe0b
Priya Sharma <priya@smartsolution.dev>
2025-08-24 18:44:41 +0300

    first commit

---
+++ Assignment_4.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+sum = num1 + num2 + num3
+product = num1 * num2 * num3
+average = sum / 3
+print("Results")
+print("Sum:", sum)
+print("Product:", product)
+print("Average:", average)
