7fb2e0bc23179d6da0a25bfbbd66014c631435b5
Priya Sharma <priya@smartsolution.dev>
2025-09-03 10:48:17 +0300

    first commit

---
+++ IN_CM_conveter.py
@@ -0,0 +1,9 @@
+#(2) Inches to Centimeters Converter.
+
+inches = float(input("Enter inches: "))
+
+while inches >= 0:
+    print(inches * 2.54)
+    inches = float(input("Enter inches: "))
+
+print("Done.")
